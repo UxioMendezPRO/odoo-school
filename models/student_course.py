f1802bff1d14a089fd6a12200172caadbfef0be5
UxioMendezPRO <uxio.mendez@hotmail.com>
2024-06-11 11:24:27 +0200

    Añadido stat button para consultar gastos de matrícula

--- models/student_course.py
+++ models/student_course.py
@@ -9,3 +9,10 @@ class Student(models.Model):
     address = fields.Char("Address")
     personal_file = fields.Char("Personal file")
     tuition_ids = fields.One2many("tuition.course", "student_id", string="Tuitions")
+    expenses = fields.Float(compute="_compute_expenses", string="Expenses", store=True)
+    
+
+    @api.depends("tuition_ids.price")
+    def _compute_expenses(self):
+        for record in self:
+            record.expenses = sum(tuition.price for tuition in record.tuition_ids)
